483d220658c4efa9f0d9dae8cda053dd55a12c52
lomiler <jonaslomiler@hotmail.com>
2017-07-19 03:06:30 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,28 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de usuários buscando pelo Login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+
+//Carrega um usuário usando o login e a senha;
+
+$usuario = new Usuario();
+$usuario->login("tio", "1234");
+
+echo $usuario;
+
 
  ?>
