9fe6980e799ed785a395576fc452801495678a33
lomiler <jonaslomiler@hotmail.com>
2017-07-19 02:14:15 -0300

    inicio projeto

---
+++ class/Sql.php
@@ -0,0 +1,47 @@
+<?php 
+
+class Sql extends PDO {
+
+	private $conn;
+
+	//Início função construtor
+	public function __construct(){
+		$this->conn = new PDO("mysql:host=localhost;dbname=test", "root", "");
+	}
+	//Fim função construtor
+
+	//Inicio função setParams
+	private function setParams($statment, $parameters = array()){
+		foreach ($parameters as $key => $value) {		
+			$this->setParam($key, $value);
+		}
+	}
+	//Fim função setParams
+
+
+	//Inicio função setParam
+	private function setParam($statment, $key, $value){
+		$statment->bindParam($key, $value);
+	}
+	//Fim função setParam
+
+
+	//Inicio função query
+	public function query($rawQuery, $params = array()){
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+		return $stmt;
+	}
+	//Fim função query.
+
+	public function select($rawQuery, $params = array()):array{
+
+		$stmt = $this->query($rawQuery, $params);
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+}
+
+ ?>
